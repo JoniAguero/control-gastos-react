3da0fba858a932100dfb8809e9ecc8942e2b706d
JoniAguero <aguerojonathanivan@gmail.com>
2018-08-25 17:45:48 -0300

    add formulario

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../css/App.css';
 import Header from './Header/Header';
+import Formulario from './Formulario/Formulario';
 
 class App extends Component {
   render() {
@@ -11,7 +12,7 @@ class App extends Component {
         <div className="contenido-principal contenido">
           <div className="row">
             <div className="one-half column">
-
+              <Formulario />
             </div>
             <div className="one-half column">
 
