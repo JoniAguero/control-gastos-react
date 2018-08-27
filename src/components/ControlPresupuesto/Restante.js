d06cf749ea6b40816309191474d1ec6fdf0ff2f2
JoniAguero <aguerojonathanivan@gmail.com>
2018-08-27 06:12:43 -0300

    Restante class
    cambia de color segun %

--- src/components/ControlPresupuesto/Restante.js
+++ src/components/ControlPresupuesto/Restante.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
+import { revisarPresupuesto } from "../../js/helper";
 
 export class Restante extends Component {
   render() {
+
+    const presupuesto = this.props.presupuesto;
+    const restante = this.props.restante;
+
     return (
-        <div className="alert alert-success">
+        <div className={revisarPresupuesto(presupuesto, restante)}>
             <p> Restante: $ {this.props.restante} </p>
         </div>
     )
